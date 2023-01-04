defbd9029bc1d9f540d1c702d99fbaca5212b9f3
topefolorunso <folorunsotemitope10@gmail.com>
2023-01-04 20:07:53 +0100

    finish pipeline

--- pipeline.py
+++ pipeline.py
@@ -1,10 +1,24 @@
 from etl import *
 
+
+
 file_path = "~/basic-etl-pipeline/data/economic-indicators.csv"
 save_path = "~/basic-etl-pipeline/data/clean_economic-indicators.csv"
 
+def run_pipeline(file_path:str, save_path:str):
+
+    # extract
+    df = extract(file_path=file_path)
+
+    # transform
+    df = transform(df=df)
+
+    # load
+    load(df=df, save_path=save_path)
+
+    return
 
 
-df = extract(file_path=file_path)
-df = transform(df=df)
-load(df=df, save_path=save_path)
+if __name__ == "__main__":
+    # run pipeline
+    run_pipeline(file_path=file_path, save_path=save_path)
